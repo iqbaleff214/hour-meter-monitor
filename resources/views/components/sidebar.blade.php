cf14fda4c1f17be4ee2309866e74d356943de567
M. Iqbal Effendi <iqbaleff214@gmail.com>
2024-05-12 05:52:39 +0800

    remove welcome page

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,7 +1,7 @@
 
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
     <div class="app-brand demo">
-      <a href="{{ route('welcome') }}" class="app-brand-link">
+      <a href="{{ route('dashboard') }}" class="app-brand-link">
         <span class="app-brand-logo demo"><img src="{{ asset('404_Black.jpg') }}" alt="404 Not Found Indonesia" width="30" style="border-radius: 150px" srcset=""></span>
         <span class="app-brand-text menu-text fw-bold ms-2 fs-5">{{ config('app.name') }}</span>
       </a>
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\PageController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PageController::class, 'welcome'])->name('welcome');
+Route::redirect('/', 'dashboard');
 
 Route::middleware('auth')->group(function () {
 
