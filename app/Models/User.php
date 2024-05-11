d1792c166279fd22dd9b16e18ce8c9a552688467
M. Iqbal Effendi <iqbaleff214@gmail.com>
2024-05-11 19:03:22 +0800

    add avatar url for user

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -35,6 +37,10 @@ class User extends Authenticatable
         'remember_token',
     ];
 
+    protected $append = [
+        'avatar_url',
+    ];
+
     /**
      * Get the attributes that should be cast.
      *
@@ -46,5 +52,11 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function avatarUrl(): Attribute {
+        return new Attribute(
+            get: fn() => 'https://ui-avatars.com/api/?name=' . $this->name,
+        );
     }
 }
